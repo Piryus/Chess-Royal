50f786122374f0346e54efab099b42db75f2fe31
lalamefine <l.triboulin@gmail.com>
2017-04-23 23:23:41 +0200

    Ajout de commentaires

--- src/defs.h
+++ src/defs.h
@@ -1,7 +1,7 @@
 #ifndef DEF
 #define DEF
 
-//Taille de la fenÍtre
+//Taille de la fen√™tre
 #define WINDOW_WIDTH 1600
 #define WINDOW_HEIGHT 900
 
@@ -102,129 +102,141 @@ typedef struct Game
     int plateau[8][8];
 } Game;
 
-//Prototypes des fonctions gÈrant le jeu
-//Fonction renvoyant la case sur laquelle on a cliquÈ
+//Prototypes des fonctions g√©rant le jeu
+//Fonction renvoyant la case sur laquelle on a cliqu√©
 extern SDL_Rect get_clicked_square(int x, int y);
- //Fonction principal gÈrant le jeu
+ //Fonction principal g√©rant le jeu
 extern void game(SDL_Renderer *renderer,Square square[][8], int ia, Game *game);
 //Fonction initialisant la position des pions
 extern void initialize_pawns_pos(Square square[][8]);
-//Fonction transposant le numÈro d'une case en coordonÈes pour le pion (y)
+//Fonction transposant le num√©ro d'une case en coordon√©es pour le pion (y)
 extern int posy(int numcase);
-//Fonction transposant le numÈro d'une case en coordonÈes pour le pion (x)
+//Fonction transposant le num√©ro d'une case en coordon√©es pour le pion (x)
 extern int posx(int numcase);
-//Fonction transposant le numÈro d'une case en abscisses (de 0 ‡ 8) en coordonÈes du coin supÈrieur gauche de cette case(x)
+//Fonction transposant le num√©ro d'une case en abscisses (de 0 √† 8) en coordon√©es du coin sup√©rieur gauche de cette case(x)
 extern int numcase_to_coord_y(int numcase);
-//Fonction transposant le numÈro d'une case en abscisses (de 0 ‡ 8) en abscisse du coin supÈrieur gauche de cette case(x)
+//Fonction transposant le num√©ro d'une case en abscisses (de 0 √† 8) en abscisse du coin sup√©rieur gauche de cette case(x)
 extern int numcase_to_coord_x(int numcase);
-//Remet ‡ 0 tous les dÈplacements possibles
+//Remet √† 0 tous les d√©placements possibles
 extern void reset_OK_moves(Square square[][8]);
-//DÈplace le pion sur la case cliquÈe
+//D√©place le pion sur la case cliqu√©e
 extern int move_pawn_to(SDL_Rect clickedSquare,Square square[][8]);
-//Ecrit les dÈplacements autorisÈs
+//Ecrit les d√©placements autoris√©s
 extern void get_authorized_moves(SDL_Rect rect,Square square[][8], int tour);
-//GËre les Èvenements (clic, etc) de la partie
+//G√®re les √©venements (clic, etc) de la partie
 extern void wait_for_event(SDL_Renderer *renderer,Square square[][8],int ia, Game *game);
 //Renvoie le gagnant de la partie (noir ou blanc ou aucun)
 extern int getWinner(SDL_Renderer *renderer, Square square[][8], Game * game);
-//Permet de vÈrifier si il y a encore des dÈplacements possibles, gËre Ègalement le cas o˘ il n'y en a pas
+//Permet de v√©rifier si il y a encore des d√©placements possibles, g√®re √©galement le cas o√π il n'y en a pas
 extern int isblocked(int color, Game *game, SDL_Renderer *renderer);
-//GËre le clic sur le bouton de retour au menu sur l'Ècran de victoire
+//G√®re le clic sur le bouton de retour au menu sur l'√©cran de victoire
 extern void wait_for_click_on_button(void);
 
-//Fonctions gÈrant l'affichage en jeu
+//Fonctions g√©rant l'affichage en jeu
 //Affiche le fond en bois
 extern void render_background(SDL_Renderer *renderer);
-//Affiche le "sous-plateau" de couleur uni permettant d'obtenir un joli Èchiquier
+//Affiche le "sous-plateau" de couleur uni permettant d'obtenir un joli √©chiquier
 extern void render_base(SDL_Renderer *renderer);
-//Affiche les cases de l'Èchiquier
+//Affiche les cases de l'√©chiquier
 extern void render_squares(SDL_Renderer *renderer);
-//Affiche les pions de l'Èchiquier
+//Affiche les pions de l'√©chiquier
 extern void render_pawns(SDL_Renderer *renderer,Square square[][8]);
-//Affiche les cases vertes et rouges o˘ un dÈplacement est possible
+//Affiche les cases vertes et rouges o√π un d√©placement est possible
 extern void render_authorized_moves(SDL_Rect clickedSquare, SDL_Renderer *renderer,Square square[][8]);
-//Affiche l'Ècran de victoire
+//Affiche l'√©cran de victoire
 extern void render_victory_screen(SDL_Renderer *renderer, int color);
-//Affiche le compteur de tour en haut de l'Ècran
+//Affiche le compteur de tour en haut de l'√©cran
 extern void render_turn(SDL_Renderer *renderer, Game *partie);
 
-//fonction gÈrant le fichier de sauvegarde
+   //fonctions g√©rant le fichier de sauvegarde
+//Permet de suprimmer une partie termin√©e du fichier de sauvegarde
 extern void supprimerPartie(int id);
+//Permet de mettre √† jour une partie du fichier de sauvegarde
 extern void Save( Game * game );
+//Permet de cr√©er une partie dans le fichier de sauvegarde
 extern void nouvellePartie(int ia, Game * partie ,int idJB,int idJN);
+//Permet de charger une partie du fichier de sauvegarde
 extern void chargerPartie(int id, Game * game);
+//Permet de placer tout le contenu du fichier de sauvegarde des parties dans un tableau
 extern int listerParties(Game parties[]);
+//Renvoie le nombre de parties enregistr√©es
 extern int saveSize();
+//Permet de cr√©er un joueur dans le fichier de sauvegarde
 extern void nouveauJoueur(char nom[]);
+//Permet de charger un joueur du fichier de sauvegarde
 extern void chargerJoueur(int id, Joueur * joueur);
+//Permet de mettre √† jour un joueur du fichier de sauvegarde
 extern void SaveJoueur( Joueur * joueur );
+//Renvoie le nombre de joueurs enregistr√©s
 extern int nbJoueur();
+//Permet de placer tout le contenu du fichier de sauvegarde des joueurs dans un tableau
 extern int listerJoueur(Joueur joueurs[]);
+//Permet de mettre √† jour un joueur du fichier de sauvegarde
 extern void modifJoueur(int ia, int winner , int njN, int njB);
 
-//Fonctions gÈnÈrales d'affichage
-//Affiche une texture donnÈe en une position donnÈe
+//Fonctions g√©n√©rales d'affichage
+//Affiche une texture donn√©e en une position donn√©e
 extern int RendTex(SDL_Texture *texture, SDL_Renderer *renderer, int x, int y);
-//Charge une texture ‡ partir d'une police, d'un texte, et d'une couleur donnÈs
+//Charge une texture √† partir d'une police, d'un texte, et d'une couleur donn√©s
 extern SDL_Texture *loadFont_Blended(SDL_Renderer *renderer, TTF_Font *police, char texte[], int red, int green, int blue);
-//Charge une texture ‡ partir d'une image donnÈe (‡ partir d'un chemin)
+//Charge une texture √† partir d'une image donn√©e (√† partir d'un chemin)
 extern SDL_Texture *loadIMG(char path[], SDL_Renderer *renderer);
 //Initialise la SDL
 extern int init(SDL_Window **window,SDL_Renderer **renderer);
-//Affiche un bouton ‡ partir d'un texte et d'un SDL_Rect (position et dimension)
+//Affiche un bouton √† partir d'un texte et d'un SDL_Rect (position et dimension)
 extern void render_button(SDL_Renderer *renderer, char text[], SDL_Rect button);
 //Renvoie 1 si le curseur est sur le bouton, 0 sinon
 extern int isCursorOnButton(SDL_Rect button);
-//Affiche un rectangle plein ‡ partir d'un SDL_Rect (position et dimension) et d'une couleur
+//Affiche un rectangle plein √† partir d'un SDL_Rect (position et dimension) et d'une couleur
 extern void renderFillRect(SDL_Renderer *renderer,SDL_Rect rect,int r,int g,int b,int a);
-//Affiche un texte centrÈ dans un rectangle
+//Affiche un texte centr√© dans un rectangle
 extern void renderTextInRect(SDL_Renderer *renderer, char text[], SDL_Rect rect);
-//Affiche un texte de couleur donnÈe centrÈ sur la position donnÈ
+//Affiche un texte de couleur donn√©e centr√© sur la position donn√©
 extern void renderCenteredText(SDL_Renderer *renderer, char text[], int x, int y, int r, int g, int b, int size);
 
 
-//Fonctions utilisÈes par le menu
+//Fonctions utilis√©es par le menu
 //Fonction principale du menu
 extern void menu(void);
 //Affiche les boutons du menu
 extern void render_menu_buttons(SDL_Renderer *renderer);
-//Affiche l'arriËre plan du menu
+//Affiche l'arri√®re plan du menu
 extern void render_menu_background(SDL_Renderer *renderer,Square square[][8]);
 //Affiche le titre du jeu (Chess Royal)
 extern void render_game_title(SDL_Renderer *renderer);
-//Attribue alÈatoirement ‡ chaque case soit un pion noir ou blanc ou rien
+//Attribue al√©atoirement √† chaque case soit un pion noir ou blanc ou rien
 extern void init_random_pawns_pos(Square square[][8]);
-//GËre l'Èvenement du clic dans le menu
+//G√®re l'√©venement du clic dans le menu
 extern int event_click(SDL_Renderer *renderer, Square square[][8], Game * partie);
 
-//Fonctions gÈrant le menu de sÈlection du joueur
-//Charge le menu de sÈlection du joueur
+//Fonctions g√©rant le menu de s√©lection du joueur
+//Charge le menu de s√©lection du joueur
 extern void select_players_menu(SDL_Renderer *renderer, int isAIGame,int *quit, int *IDPlayerB, int *IDPlayerW);
 //Affiche la liste de joueurs
 extern void render_playersList(SDL_Renderer *renderer, int adder);
-//Charge le menu de crÈation d'un joueur
+//Charge le menu de cr√©ation d'un joueur
 extern void render_newPlayerMenu(SDL_Renderer *renderer);
 //Associe aux deux joueurs de la partie l'ID du joueur choisi dans le menu
 extern void selectPlayer(int ID, int selectedPlayerNb, int *IDPlayerB, int *IDPlayerW);
 
-//Fonctions gÈrant le menu de chargement d'une partie
-//Charge le menu de sÈlection d'une partie
+//Fonctions g√©rant le menu de chargement d'une partie
+//Charge le menu de s√©lection d'une partie
 extern int load_game_menu_and_get_choice(SDL_Renderer *renderer, Game partiesList[]);
-//Charge l'entÍte du tableau des parties
+//Charge l'ent√™te du tableau des parties
 extern void load_menu_header(SDL_Renderer *renderer, int adder);
 //Affiche les dalles des parties
 extern void render_games_buttons(SDL_Renderer *renderer, int adder);
 //Affiche les infos sur les parties
 extern void render_games_infos(SDL_Renderer *renderer, Game partiesList[], int adder);
-//GËre le choix de l'utilisateur
+//G√®re le choix de l'utilisateur
 extern int get_choice(int adder);
 //Affiche le bouton pour retourner au menu
 extern void render_back_to_menu_button(SDL_Renderer *renderer, int adder);
 
-//Fonctions gÈrant le menu des scores
+//Fonctions g√©rant le menu des scores
 //Charge le menu des scores
 extern void scores_menu(SDL_Renderer *renderer);
-//Affiche l'entÍte du tableau des scores
+//Affiche l'ent√™te du tableau des scores
 extern void renderScoreMenuHeader(SDL_Renderer *renderer, int adder);
 //Affiche les dalles des joueurs
 extern void renderPlayersTiles(SDL_Renderer *renderer, int adder);
@@ -233,9 +245,15 @@ extern void renderPlayerInfos(SDL_Renderer *renderer, int adder);
 
 
 //Fonctions de l'IA
+//fonction qui renvoie la valeur de la case du tableau plt, mais en pr√©voyant un code d'erreur pour les requettes sortants du tableau
 extern int arrAcc(int plt[][8],int x, int y);
+//fonction qui calcule le gain relatif d'effectuer un d√©placement
 extern float poids(int color,Square plt[][8],int frmX,int frmY,int toX,int toY,JoueurIA * jN);
+//fonction qui √©tablit la liste des action possibles de l'ia
 extern int listActions(Possibillite actionlist[] , Square plateau[][8], JoueurIA * jN);
+//fonction qui s√©lectionne la meilleure action que l'IA peut effectuer
 extern int findBestAction(Possibillite *bestAction , Square plateau[][8],JoueurIA * jN);
+//fonction de deplacement con√ßue pour l'ia et ses calculs
 extern void deplacement(Possibillite dpl, Square plat[][8],int color);
+//fonction de debug permettant d'afficher le tableau
 extern void affPlt(int plt[][8]);
