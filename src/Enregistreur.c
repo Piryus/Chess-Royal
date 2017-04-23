ccc163b99341846c6f288345385c35dc5369c1dc
lalamefine <l.triboulin@gmail.com>
2017-04-23 19:59:11 +0200

    Fix des accès aux joueurs

--- src/Enregistreur.c
+++ src/Enregistreur.c
@@ -28,19 +28,19 @@ void nouveauJoueur(char nom[]){
     FILE *fic;
     fic = fopen("JoueurListe.dat","r");
     Joueur X;
-    int maxid = 0;
+    int maxid = -1;
     if(fic != NULL){
-            fread(&X, sizeof(Joueur), 1, fic);
+        fread(&X, sizeof(Joueur), 1, fic);
         while(!feof(fic)){
             if(X.id>maxid){maxid = X.id;}
             fread(&X, sizeof(Joueur), 1, fic);
         }
         fclose(fic);
     }
-    printf("MAX - ID : %d \n",maxid);
+    printf("MAX - JID : %d \n",maxid);
     //création du joueur
     Joueur newPlayer;
-    newPlayer.id = maxid;
+    newPlayer.id = maxid+1;
     strcpy(newPlayer.Nom,nom);
     newPlayer.nbEgal = 0;
     newPlayer.nbWin = 0;
@@ -53,7 +53,7 @@ void nouveauJoueur(char nom[]){
         fic = fopen("JoueurListe.dat","w+");
         printf("Création du fichier de sauvegarde\n");
     }
-    printf("Creation du joueur %s ", nom);
+    printf("Creation du joueur %s \n", nom);
     fwrite(&newPlayer, sizeof(Joueur), 1, fic);
     fclose(fic);
 }
@@ -62,11 +62,18 @@ void chargerJoueur(int id, Joueur * joueur){
     FILE *fic;
     fic = fopen("JoueurListe.dat","r");
     if(fic != NULL){
-        while((joueur->id != id)&&(feof(fic)!=1)){
-            fread(joueur, sizeof(joueur), 1, fic);
+        fread(joueur, sizeof(Joueur), 1, fic);
+        while((joueur->id != id)&&(!feof(fic))){
+            printf("    > %s  ( ID : %2d)\n",joueur->Nom,joueur->id);
+            fread(joueur, sizeof(Joueur), 1, fic);
         }
-        if(joueur->id != id)
+        printf("    > %s ( ID : %2d)\n",joueur->Nom,joueur->id);
+        if(joueur->id != id){
             joueur->id = -1;
+            printf("Impossible de charger le joueur\n");
+        }else{
+            printf("Joueur %s Charge ( ID : %2d)\n",joueur->Nom,joueur->id);
+        }
     }else{
         joueur->id = -1;
     }
@@ -75,45 +82,48 @@ void chargerJoueur(int id, Joueur * joueur){
 }
 
 void SaveJoueur( Joueur * joueur ){
-    printf("----------------  SAVE J -----------------\n");
+    printf("----------------  SAVE J ----------------");
     FILE *fic;
-    int size = saveSize();
+    int size = nbJoueur();
    // printf("    Size : %d \n",size);
     //on charge tout dans un meme tableau
     fic = fopen("JoueurListe.dat","r");
     Joueur ljoueur[size];
     for(int a = 0; a<size ; a++){
         fread(&ljoueur[a], sizeof(Joueur), 1, fic);
-        printf("fread %d : j[id:%d]  \n",a,ljoueur[a].id);
     }
     fclose(fic);
-    //on cherche la joueur apropriée
+    //on cherche le joueur approprié
+    printf("\nrecherche du joueur %s\n",joueur->Nom);
     int i = 0;
     while(ljoueur[i].id != joueur->id){
+        printf("    %3d>joueur %s  W:%2d  L:%2d  E:%2d  G:%2d \n",ljoueur[i].id,ljoueur[i].Nom,ljoueur[i].nbWin,ljoueur[i].nbLose,ljoueur[i].nbEgal,ljoueur[i].nbGame);
         i++;
     }
-    //printf(" id %d = id %d >OK\n",ljoueur[i].id ,joueur->id);
-    //on modifie la joueur apropriée
-    ljoueur[i].id = joueur->id;
-    strcpy(ljoueur[i].Nom,joueur->Nom);
+    printf("    %3d>joueur %s  W:%2d  L:%2d  E:%2d  G:%2d \n",ljoueur[i].id,ljoueur[i].Nom,ljoueur[i].nbWin,ljoueur[i].nbLose,ljoueur[i].nbEgal,ljoueur[i].nbGame);
+    printf("Affectation des nouvelle valeurs : \n");
+    //ljoueur[i].id = joueur->id;
+    //strcpy(ljoueur[i].Nom,joueur->Nom);
+
     ljoueur[i].nbWin = joueur->nbWin;
     ljoueur[i].nbLose = joueur->nbLose;
     ljoueur[i].nbGame = joueur->nbGame;
     ljoueur[i].nbEgal = joueur->nbEgal;
+    printf("    %3d>joueur %s  W:%2d  L:%2d  E:%2d  G:%2d \n",ljoueur[i].id,ljoueur[i].Nom,ljoueur[i].nbWin,ljoueur[i].nbLose,ljoueur[i].nbEgal,ljoueur[i].nbGame);
     //on réécrit tout dans le fichier
+    remove("JoueurListe.dat");
     fic = fopen("JoueurListe.dat","w+");
    // printf("rewrite all \n");
     for(int a = 0; a<size ; a++){
         fwrite(&ljoueur[a], sizeof(Joueur), 1, fic);
     }
     fclose(fic);
-   // printf("--------------  END SAVE  ---------------\n");
+    printf("X\n");
 }
 
 void modifJoueur(int ia, int winner , int njN, int njB){
     Joueur jB;
     chargerJoueur(njB, &jB);
-    //chargerJoueur(1, &jB);
     if(winner == _BLANC){jB.nbWin++;jB.nbGame++;}
     if(winner == _NOIR){jB.nbLose++;jB.nbGame++;}
     if(winner == _VIDE){jB.nbEgal++;jB.nbGame++;}
@@ -121,7 +131,6 @@ void modifJoueur(int ia, int winner , int njN, int njB){
     if(ia == 0){
         Joueur jN;
         chargerJoueur(njN, &jN);
-        //chargerJoueur(2, &jN);
         if(winner == _BLANC){jN.nbWin++;jN.nbGame++;}
         if(winner == _NOIR){jN.nbLose++;jN.nbGame++;}
         if(winner == _VIDE){jN.nbEgal++;jN.nbGame++;}
@@ -153,18 +162,17 @@ int listerJoueur(Joueur joueurs[]){ // OK ; écrit la liste des joueurs dans le t
         int size = nbJoueur();
         for(int a = 0; a<size ; a++){
             fread(&joueurs[a], sizeof(Joueur), 1, fic);
-            printf("fread %d : j[id:%d]  \n",a,joueurs[a].id);
         }
         fclose(fic);
         return 0;
     }else{
-        printf("Pas de Joueur sauvegardé");
+        printf("Pas de Joueur sauvegarde\n");
         return 1;
     }
 }
 
 void Save( Game * partie ){  //OK
-    printf("----------------  SAVE  -----------------\n");
+    printf("----------------  SAVE  -----------------");
     FILE *fic;
     int size = saveSize();
    // printf("    Size : %d \n",size);
@@ -173,7 +181,6 @@ void Save( Game * partie ){  //OK
     Game lPart[size];
     for(int a = 0; a<size ; a++){
         fread(&lPart[a], sizeof(Game), 1, fic);
-        printf("fread %d : [id:%d]  \n",a,lPart[a].id);
     }
     fclose(fic);
     //on cherche la partie apropriée
@@ -201,7 +208,7 @@ void Save( Game * partie ){  //OK
         fwrite(&lPart[a], sizeof(Game), 1, fic);
     }
     fclose(fic);
-   // printf("--------------  END SAVE  ---------------\n");
+    printf("X\n");
 }
 //***************************************************************************************************************
 void nouvellePartie(int ia, Game * partie ,int idJB,int idJN){// OK
@@ -218,7 +225,7 @@ void nouvellePartie(int ia, Game * partie ,int idJB,int idJN){// OK
         }
         fclose(fic);
     }
-    printf("MAX - ID : %d \n",maxid);
+    printf("MAX - GID : %d \n",maxid);
     //création de la partie
     partie->id = maxid+1;
     partie->ia = ia;
@@ -244,7 +251,7 @@ void nouvellePartie(int ia, Game * partie ,int idJB,int idJN){// OK
         fic = fopen("PartiesListe.dat","w+");
         printf("Création du fichier de sauvegarde\n");
     }
-    printf("Creation de la sauvegarde %d (IA:%d)", partie->id , partie->ia);
+    printf("Creation de la sauvegarde %d (IA:%d)\n", partie->id , partie->ia);
     fwrite(partie, sizeof(Game), 1, fic);
     fclose(fic);
 }
@@ -253,6 +260,7 @@ void chargerPartie(int id, Game * partie){ // OK
     FILE *fic;
     fic = fopen("PartiesListe.dat","r");
     if(fic != NULL){
+        fread(partie, sizeof(Game), 1, fic);
         while((partie->id != id)&&(feof(fic)!=1)){
             fread(partie, sizeof(Game), 1, fic);
         }
@@ -273,7 +281,6 @@ int listerParties(Game parties[]){ // OK ; écrit la liste des partie dans le tab
         int size = saveSize();
         for(int a = 0; a<size ; a++){
             fread(&parties[a], sizeof(Game), 1, fic);
-            printf("fread %d : [id:%d]  \n",a,parties[a].id);
         }
         fclose(fic);
         return 0;
@@ -284,7 +291,7 @@ int listerParties(Game parties[]){ // OK ; écrit la liste des partie dans le tab
 }
 
 void supprimerPartie(int id){
-printf("----------------  SUPP  -----------------\n");
+printf("----------------  SUPP  -----------------");
 
     FILE *fic;
     int size = saveSize();
@@ -294,7 +301,6 @@ printf("----------------  SUPP  -----------------\n");
     Game lPart[size];
     for(int a = 0; a<size ; a++){
         fread(&lPart[a], sizeof(Game), 1, fic);
-        printf("fread %d : [id:%d]  \n",a,lPart[a].id);
     }
     fclose(fic);
     //on cherche la partie apropriée
@@ -316,6 +322,7 @@ printf("----------------  SUPP  -----------------\n");
                 lPart[i].plateau[c][l] = lPart[i+1].plateau[c][l];
             }
         }
+        i++;
     }
     size--;
     //on réécrit tout dans le fichier
@@ -325,7 +332,7 @@ printf("----------------  SUPP  -----------------\n");
         fwrite(&lPart[a], sizeof(Game), 1, fic);
     }
     fclose(fic);
-   // printf("--------------  END SAVE  ---------------\n");
+    printf("X\n");
 }
 //***************************************************************************************************************
 int saveSize(){
