414f4c4ad502ccc11ae0b87a69028de4ac3f300b
Piryus <piryus@gmail.com>
2017-04-23 21:06:31 +0200

    Adds scrolling to scores menu

--- src/scores_menu.c
+++ src/scores_menu.c
@@ -5,29 +5,59 @@ void scores_menu(SDL_Renderer *renderer)
     Square square[8][8];
     SDL_Event event;
     int stop=0;
-    SDL_Rect goBackButton={WINDOW_WIDTH-(BUTTON_WIDTH-150)-10, 20,BUTTON_WIDTH-150,BUTTON_HEIGHT};
+    int adder=0;
+    SDL_Rect goBackButton= {WINDOW_WIDTH-(BUTTON_WIDTH-150)-10, 20,BUTTON_WIDTH-150,BUTTON_HEIGHT};
     SDL_RenderClear(renderer);
     render_menu_background(renderer,square);
+    goBackButton.y=20+adder;
     render_button(renderer,"Retour", goBackButton);
-    renderScoreMenuHeader(renderer);
-    renderPlayersTiles(renderer);
-    renderPlayerInfos(renderer);
+    renderScoreMenuHeader(renderer,adder);
+    renderPlayersTiles(renderer,adder);
+    renderPlayerInfos(renderer,adder);
     while(!stop)
     {
-       SDL_WaitEvent(&event);
-       switch(event.type)
-       {
-       case SDL_MOUSEBUTTONDOWN:
-        if(isCursorOnButton(renderer,goBackButton))
+        SDL_WaitEvent(&event);
+        switch(event.type)
         {
-            stop=1;
+        case SDL_MOUSEBUTTONDOWN:
+            if(isCursorOnButton(renderer,goBackButton))
+            {
+                stop=1;
+            }
+            break;
+        case SDL_KEYDOWN:
+            switch(event.key.keysym.sym)
+            {
+            case SDLK_DOWN:
+                adder=adder-100;
+                SDL_RenderClear(renderer);
+                render_menu_background(renderer,square);
+                goBackButton.y=20+adder;
+                render_button(renderer,"Retour", goBackButton);
+                renderScoreMenuHeader(renderer,adder);
+                renderPlayersTiles(renderer,adder);
+                renderPlayerInfos(renderer,adder);
+                break;
+            case SDLK_UP:
+                if(adder<=-100)
+                {
+                    adder=adder+100;
+                    SDL_RenderClear(renderer);
+                    render_menu_background(renderer,square);
+                    goBackButton.y=20+adder;
+                    render_button(renderer,"Retour", goBackButton);
+                    renderScoreMenuHeader(renderer,adder);
+                    renderPlayersTiles(renderer,adder);
+                    renderPlayerInfos(renderer,adder);
+                }
+                break;
+            }
+            break;
         }
-        break;
-       }
     }
 }
 
-void renderScoreMenuHeader(SDL_Renderer *renderer)
+void renderScoreMenuHeader(SDL_Renderer *renderer, int adder)
 {
     SDL_Texture *header;
     SDL_Rect font_rect;
@@ -38,16 +68,16 @@ void renderScoreMenuHeader(SDL_Renderer *renderer)
     {
         header = loadFont_Blended(renderer, font_OpenSans, text[i], 39, 174, 96);
         SDL_QueryTexture(header, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(header, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * (i + 1), (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2);
+        RendTex(header, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * (i + 1), (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2+adder);
         SDL_RenderPresent(renderer);
     }
 }
 
-void renderPlayersTiles(SDL_Renderer *renderer)
+void renderPlayersTiles(SDL_Renderer *renderer, int adder)
 {
     SDL_Rect tile = {(WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2,
-                     (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 50,
-                     WINDOW_WIDTH / 1.1,
+                     (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 50+adder,
+                     WINDOW_WIDTH / 1.1 +adder,
                      100
                     };
     int size = nbJoueur();
@@ -61,7 +91,7 @@ void renderPlayersTiles(SDL_Renderer *renderer)
     SDL_RenderPresent(renderer);
 }
 
-void renderPlayerInfos(SDL_Renderer *renderer)
+void renderPlayerInfos(SDL_Renderer *renderer, int adder)
 {
     int size = nbJoueur();
     Joueur players[size];
@@ -86,19 +116,19 @@ void renderPlayerInfos(SDL_Renderer *renderer)
     {
         text = loadFont_Blended(renderer, font_OpenSans, name[j], 142, 68, 173);
         SDL_QueryTexture(text, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30);
+        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30 +adder);
         text = loadFont_Blended(renderer, font_OpenSans, wins[j], 142, 68, 173);
         SDL_QueryTexture(text, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 2, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30);
+        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 2, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30 +adder);
         text = loadFont_Blended(renderer, font_OpenSans, losses[j], 142, 68, 173);
         SDL_QueryTexture(text, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 3, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30);
+        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 3, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30 +adder);
         text = loadFont_Blended(renderer, font_OpenSans, ties[j], 142, 68, 173);
         SDL_QueryTexture(text, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 4, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30);
+        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 4, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30+adder);
         text = loadFont_Blended(renderer, font_OpenSans, total[j], 142, 68, 173);
         SDL_QueryTexture(text, NULL, NULL, &font_rect.w, &font_rect.h);
-        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 5, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30);
+        RendTex(text, renderer, (WINDOW_WIDTH - WINDOW_WIDTH / 1.1) / 2 + 200 * 5, (WINDOW_HEIGHT - WINDOW_HEIGHT / 1.1) / 2 + 110 * (j + 1) - 30+adder);
     }
     SDL_RenderPresent(renderer);
 }
